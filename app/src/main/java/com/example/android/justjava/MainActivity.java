ee6b6bf9a3180dfc5f5f8c314e5f11c99a0cf719
computerzman <mhe.developer@gmail.com>
2018-02-01 22:15:32 +0200

    Lesson-7-MakingAnAppInteractivePart2
    Lesson-7-MakingAnAppInteractivePart2

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int qauntity = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        //int numberOfCoffees = 2;
+      //  display(numberOfCoffees);
+        String priceMessage = "Amount Due " + qauntity*5;
+      //  displayPrice(qauntity*5);
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * This Method to increment quantity
+     **/
+    public void increment(View view) {
+        qauntity = qauntity  + 1;
+        display(qauntity);
+    }
+
+
+    /**
+     * This Method to decrement quantity
+     **/
+    public void decrement(View view) {
+        qauntity = qauntity  - 1;
+        display(qauntity);
+    }
+
+
+
+}
